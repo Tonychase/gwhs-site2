751e66c5f0f902c3cf08399e42d2fcada8df9642
Tonychase <nphill1922@withs.org>
2015-02-09 09:55:20 -0500

    Create guts.js

---
+++ js/guts.js
@@ -0,0 +1,61 @@
+function divide(x, y)
+
+{
+
+    return x/y;
+
+}
+
+ function add(x, y)
+ 
+ {
+     
+    return x+y;
+ 
+ }
+ 
+ function multiply(x, y)
+ 
+ {
+     
+    return x*y;
+
+ }
+ 
+ function subtract(x, y)
+ 
+ {
+     
+   return x-y;
+  
+ }
+ 
+  function squre_root (x)
+ 
+ {
+   
+   return Math.sqrt(x) 
+
+ }
+ 
+ function power (x, y)
+
+ {
+  
+  return Math.pow(x, y) 
+
+}
+ 
+ function logBase10(x)
+{
+ 
+  return Math.log(x)
+}
+
+function generateRandomNumber(x)
+
+{
+ 
+  return Math.random(x)
+
+}
